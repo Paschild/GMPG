0fdd8ae7f677f203085536f60c2f5bc64e8dbf60
eafschad <53854818+eafschad@users.noreply.github.com>
2019-09-30 14:33:07 +0200

    Update Classes.py

--- E/Classes.py
+++ E/Classes.py
@@ -1,4 +1,4 @@
-'''class Auto():
+class Auto():
 
     def get_info(self):
         print("El auto es " + self.color + " y de " + self.ano)
@@ -22,7 +22,7 @@ audi.get_info() '''     #Clase con atributos y función
 
 #Clases con Constructor(__init__):
 
-'''class Auto():
+class Auto():
 
     def __init__(self, color, ano):
         self.color = color
@@ -37,13 +37,13 @@ toyota = Auto("rojo", "1995")
 toyota.get_info()
 
 audi = Auto("negro", "2005")
-audi.get_info()'''
+audi.get_info()
 
 
 
 
 
-'''class Cliente():
+class Cliente():
 
     def __init__ (self, nombre, apellido):
         self.nombre = nombre
@@ -57,4 +57,56 @@ maria = Cliente("Maria", "Perez")
 maria.get_name()
 
 jose = Cliente("Jose", "Martinez")
-jose.get_name()'''
+jose.get_name()
+
+
+
+
+
+class Suma:
+    primer_num = 0
+    segundo_num = 0
+    resultado = 0
+
+    def __init__(self, p, s):
+        self.primer_num = p
+        self.segundo_num = s
+
+    def display(self):
+        print ("Primer numero = " + str(self.primer_num))
+        print ("Segundo numero = " + str(self.segundo_num))
+        print ("Resultado = " + str(self.resultado))
+
+    def calculo(self):
+        self.resultado = self.primer_num + self.segundo_num
+
+num_uno = Suma(1, 2)
+num_uno.calculo()
+num_uno.display()
+
+
+num_dos = Suma(3, 4)
+num_dos.calculo()
+num_dos.display()'''
+
+
+
+class Autor:
+    def __init__(self, nombre, genero, ano):
+        self.nombre = nombre
+        self.genero = genero
+        self.ano = ano
+
+    def incremento_ano(self):
+        self.ano = self.ano + 1
+
+    def display(self):
+        print ("Este autor se llama " + self.nombre + ", escribia " + self.genero + " y nacio en " + str(self.ano))
+
+borges = Autor("Borges", "ficcion", 1899)
+borges.display()
+
+octavio = Autor("Octavio", "ensayos", 1914)
+octavio.display()
+
+#Clase con Constructor, cada vez que un objeto se cree en la clase, se acitva esta funcion. Las variables seran definidad por el argumento del objeto.
